47b047cf01e1a89f12a3918781dabbf1032c95c9
Sourave Halder <souravh093@gmail.com>
2025-11-08 23:36:20 +0600

    feat: add database seeding script and enhance word retrieval service
    - Implemented a new seeding script (scripts/seed.ts) to populate the database with words, definitions, translations, and metadata from a JSON file.
    - Enhanced the getAllWordsFromDB function in word.service.ts to include related data such as base words, definitions with examples, translations with definitions and examples, and metadata when retrieving words from the database.

--- src/services/word.service.ts
+++ src/services/word.service.ts
@@ -1,7 +1,31 @@
 import prisma from "../prisma";
 
 const getAllWordsFromDB = async () => {
-  const response = await prisma.word.findMany();
+  const response = await prisma.word.findMany({
+    include: {
+      baseWord: {
+        include: {
+          languageSpecific: true,
+        },
+      },
+      definitions: {
+        include: {
+          examples: true,
+        },
+      },
+      translations: {
+        include: {
+          definitions: {
+            include: {
+              examples: true,
+            },
+          },
+          languageSpecific: true,
+        },
+      },
+      metadata: true,
+    },
+  });
 
   return response;
 };
